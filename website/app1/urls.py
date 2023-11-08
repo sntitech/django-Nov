aa0e7977c63dcac848400e754916eb62c133f0bf
Sagar Jadhav <sagar.jadhav@datahash.com>
2023-11-08 09:33:01 +0530

    new routes added

--- website/app1/urls.py
+++ website/app1/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
-from .views import home
+from .views import home, contact, services
 
 urlpatterns = [
-    path('', home, name="home")
+    path('', home, name="home"),
+    path('contact', contact, name="contact"),
+    path('services', services, name="services")
 ]
--- website/app1/views.py
+++ website/app1/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def services(request):
+    return render(request, "services.html")
