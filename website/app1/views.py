2940aa2680ba223b7e52d9636866a66311339f3a
Sagar Jadhav <sagar.jadhav@datahash.com>
2023-11-16 09:50:35 +0530

    Http methods added

--- website/app1/views.py
+++ website/app1/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+import requests
 
 def home(request):
-    return render(request, "home.html")
+    response = requests.get("https://jsonplaceholder.typicode.com/users")
+    response_obj = {
+        "users": response.json()
+    }
+    return render(request, "home.html", context=response_obj)
 
 def contact(request):
     return render(request, "contact.html")
